a65c453ecf761e3ede18d010be18de906349204e
Mauri <mauri.monzon91@gmail.com>
2025-07-25 16:50:16 -0300

    Agregado facivon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "FotoNube",
+  description: "Hecho con amor por la comunidad de FotoNube",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
@@ -27,5 +27,5 @@ html {
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
